606452e0dcfa2ce4d58297b44000237c96421513
Benjamin Gutierrez <mrbengutierrez@gmail.com>
2023-11-12 19:46:25 -0500

    python3.8 w/ tensorflow2.6.0 (#2)

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -3,6 +3,7 @@ import numpy as np
 from sitetack.alphabet import Alphabet
 from sitetack.kmer import Kmer
 from sitetack.predict import Predict
+from pathlib import Path
 
 class TestPredict:
 
@@ -10,6 +11,8 @@ class TestPredict:
         cls.alphabet_22  = Alphabet("ARNDCEQGHILKMFPSTWYVXZ-U")
 
         cls.kmer_arn = Kmer(site=42, subsequence="ARN")
+        
+        cls.model_file = Path(__file__).parent / 'resources' / 'cnn.h5'
 
 
     def test_to_indices_three_characters(self):
@@ -37,6 +40,6 @@ class TestPredict:
     def test_on_kmer_returns_valid_probability(self):
         kmer = Kmer(site=7, subsequence='----------MVPKLFTSQICLLLLLGLMGVEGSL')
         alphabet_24 = Alphabet("ARNDCEQGHILKMFPSTWYVXZ-U")
-        probability = Predict.on_kmer(kmer, alphabet_24)
+        probability = Predict.on_kmer(kmer, alphabet_24, self.model_file)
         assert 0 <= probability <= 1
     
